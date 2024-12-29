a37363fd79c6bc8782c82def6871a1ce22969d7c
MayuriSalunke34 <salunkemayuri2705@gmail.com>
2024-12-29 17:48:37 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,7 @@
 class Hello{
   public static void main (String[]args){
-    System.out.println("HelloWorld");
-    System.out.println("sakshi");
+    int a=10+20;
+    System.out.println("sum "+a);
+    
   }
 }
